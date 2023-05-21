cbab31d9d72082e5b9ae0316dc86805fdb5d602a
LucasMonir <lucasmonir@hotmail.com>
2023-05-21 20:29:18 -0300

    First commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<h1>TestSvelte</h1>
+<ul>
+    <li><a href="/about">About</a></li>
+    <li><a href="/contact">Contact</a></li>
+    <li><a href="/doubts">Doubts</a></li>
+    <li><a href="/jeff">Jeff</a></li>
+</ul>
+
+<slot />
