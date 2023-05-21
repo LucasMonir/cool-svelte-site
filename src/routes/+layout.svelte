4c4f1ffa42723845a969807f4b4949b46fbee8db
LucasMonir <lucasmonir@hotmail.com>
2023-05-21 20:39:44 -0300

    added tailwind forms and typography

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
+<script>
+  import "../app.postcss";
+</script>
+
 <h1>TestSvelte</h1>
 <ul>
-    <li><a href="/about">About</a></li>
-    <li><a href="/contact">Contact</a></li>
-    <li><a href="/doubts">Doubts</a></li>
-    <li><a href="/jeff">Jeff</a></li>
+  <li><a href="/about">About</a></li>
+  <li><a href="/contact">Contact</a></li>
+  <li><a href="/doubts">Doubts</a></li>
+  <li><a href="/jeff">Jeff</a></li>
 </ul>
 
 <slot />
