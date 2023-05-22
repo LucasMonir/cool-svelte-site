9a74dcbb920d344bd4d79e0ed21ca132f1e258cc
LucasMonir <lucasmonir@hotmail.com>
2023-05-22 09:24:57 -0300

    Control structures! cool

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -1,7 +1,15 @@
-<a href="" class="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">home</a>
+<script>
+        const links = [
+                { name: "Home", route: "" },
+                { name: "About", route: "/about" },
+                { name: "Contact", route: "/contact" },
+                { name: "Doubts", route: "/doubts" },
+        ];
+        const normalClass =
+                "text-gray-800 transition-colors duration-300 transform dark:text-gray-200 mx-1.5 sm:mx-6 ";
+        const activeClass = normalClass + "border-b-2 border-blue-500";
+</script>
 
-        <a href="/about" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">About</a>
-
-        <a href="/contact" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Contact</a>
-
-        <a href="doubts" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Doubts</a>
+{#each links as { name, route }, i}
+        <a href={route} class={i == 0 ? activeClass : normalClass}>{name}</a>
+{/each}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    const list = ["cleiton", "john", "cleberson"];
+</script>
+
+<h1>HomePage</h1>
+
+{#each list as name}
+    <p>{name}</p>
+{/each}
