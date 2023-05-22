79390a53bd7b0ade5364ce4b90def2e8acd58b18
LucasMonir <lucasmonir@hotmail.com>
2023-05-22 10:14:34 -0300

    Created item component

---
+++ src/lib/ListItem.svelte
@@ -0,0 +1,34 @@
+<script>
+    export let category = '';
+    export let title = '';
+    export let image = '';
+</script>
+
+<section class="bg-white dark:bg-gray-900">
+    <div class="container py-10 mx-auto">
+        <section class="mt-8 space-y-8 lg:mt-12">
+                <section class="lg:flex lg:items-center">
+                    <div class="lg:w-1/2">
+                        <p
+                            class="text-lg tracking-wider text-blue-500 uppercase dark:text-blue-400"
+                        >
+                            {category}
+                        </p>
+                        <h2
+                            class="mt-2 text-2xl font-semibold text-gray-800 capitalize dark:text-white"
+                        >
+                            {title}
+                        </h2>
+                    </div>
+
+                    <div class="mt-4 lg:w-1/2 lg:mt-0">
+                        <img
+                            class="object-cover w-full h-64 rounded-lg md:h-96"
+                            src={image}
+                            alt=""
+                        />
+                    </div>
+                </section>
+        </section>
+    </div>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>HomePage</h1>
+<script>
+    import ListItem from "$lib/ListItem.svelte";
 
+    const items = [
+        { title: "Amanita Muscaria", category: "Species", image: "https://www.naturezadivina.com.br/media/wysiwyg/pode-comer-amanita-muscaria.jpg" },
+        { title: "Amanita Phalloides", category: "Species", image: "https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcSjfmn3dnqwqbz-trVZPIF4nX9SIVKGJxa0wfVjQ1CJAx9-zUmHuEXmvMKdhfI4veqUrzFxce1MhFjEkLA" }
+        ];
+</script>
+
+{#each items as {title, category, image}}
+    <ListItem {category} {title} {image}></ListItem>
+{/each}
